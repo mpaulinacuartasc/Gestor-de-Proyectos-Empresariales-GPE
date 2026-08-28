cc286d17d6eeaeec4b5fea9db985bd207dde264f
Paulina Cuartas <paulinacuartas2005@gmail.com>
2026-08-28 08:12:20 -0500

    se crea el proyecto

---
+++ src/main/java/com/example/gdestor_de_proyectos_empresariales_GPE/GdestorDeProyectosEmpresarialesGpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdestor_de_proyectos_empresariales_GPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdestorDeProyectosEmpresarialesGpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdestorDeProyectosEmpresarialesGpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gdestor_de_proyectos_empresariales_GPE/GdestorDeProyectosEmpresarialesGpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdestor_de_proyectos_empresariales_GPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdestorDeProyectosEmpresarialesGpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
